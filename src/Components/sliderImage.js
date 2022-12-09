beaf6cf14b5f397e3389bc961b337a72011242b8
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-12-09 14:27:14 -0500

    updates

---
+++ src/Components/sliderImage.js
@@ -0,0 +1,27 @@
+import First from "/public/data/carousel/carousel/carousel1.png";
+import Second from "/public/data/carousel/carousel/carousel2.png";
+import Third from "/public/data/carousel/carousel/carousel3.png";
+import Fourth from "/public/data/carousel/carousel/carousel4.png";
+import Fifth from "/public/data/carousel/carousel/carousel5.png";
+import Sixth from "/public/data/carousel/carousel/carousel6.png";
+
+export default [
+  {
+    urls: First
+  },
+  {
+    urls: Second
+  },
+  {
+    urls: Third
+  },
+  {
+    urls: Fourth
+  },
+  {
+    urls: Fifth
+  },
+  {
+    urls: Sixth
+  }
+];
